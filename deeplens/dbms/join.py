a0eef020ca08ec9fca6d122472b00fdda70d158a
Sanjay Krishnan <sanjaykrishn@gmail.com>
2018-08-11 16:46:03 -0700

    Updated with an in-memory spatial join

--- deeplens/dbms/join.py
+++ deeplens/dbms/join.py
@@ -2,6 +2,8 @@ from utils import get_logger
 
 from deeplens.dbms.expressions import EqualityExpression
 
+from sklearn.neighbors import BallTree
+
 logger = get_logger(__name__)
 
 
@@ -15,7 +17,7 @@ class NestedLoopJoin(object):
 
     def read(self, args={}):
         for patch1 in self.left.read():
-            for patch2 in self.left.read():
+            for patch2 in self.right.read():
                 if self.predicate.predicate(patch1,patch2):
                     yield (patch1,patch2)
 
@@ -34,6 +36,31 @@ class IndexLoopJoin(object):
             value = patch1.metadata[self.predicate.attr1]
             eq = EqualityExpression(self.predicate.attr2,value)
 
-            for patch2 in self.left.read({'predicate': eq}):
+            for patch2 in self.right.read({'predicate': eq}):
                 if self.predicate.predicate(patch1,patch2):
                     yield (patch1,patch2)
+
+
+class MemorySpatialJoin(object):
+
+    def __init__(self, left, right, predicate, leaf_size=40):
+        logger.debug("set up the join object")
+        self.left = left
+        self.right = right
+        self.predicate = predicate
+        self.leaf_size = leaf_size
+
+    def read(self, args={}):
+
+        X = []
+
+        for patch in self.right.read():
+            X.append(patch.patch)
+
+        Y = np.array(X)
+        tree = BallTree(Y, leaf_size)
+
+        for patch in self.left.read():
+            for ind in tree.query_radius(patch.patch, predicate.thresh):
+                yield Y[i]
+            
